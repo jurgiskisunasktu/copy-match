d854ec454862b1445068bcc5eb65362c70fb30be
Jurgis Kisunas <jurgis.kisunas@elinta.lt>
2020-05-27 00:15:46 +0300

    Naming convention converter

---
+++ CopyMatch/NamingConventionsConverters/NamingConventionConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace CopyMatch.NamingConventionsConverters
+{
+    public class NamingConventionConverter
+    {
+        private CamelCase camelCase;
+        private KebabCase kebabCase;
+        private SnakeCase snakeCase;
+
+        public NamingConventionConverter()
+        {
+            camelCase = new CamelCase();
+            kebabCase = new KebabCase();
+            snakeCase = new SnakeCase();
+        }
+
+        public INamingConvention GetNamingConvention(string propertyName)
+        {
+            if (camelCase.IsTypeOfThis(propertyName)) return camelCase;
+            if (kebabCase.IsTypeOfThis(propertyName)) return kebabCase;
+            if (snakeCase.IsTypeOfThis(propertyName)) return snakeCase;
+            return null;
+        }
+    }
+}
