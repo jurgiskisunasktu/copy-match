1c022a7c8b74888abbf5708569d48b4240503c03
Jurgis Kisunas <jurgis.kisunas@elinta.lt>
2020-05-26 23:48:57 +0300

    Snake case tests

---
+++ CopyMatch.Tests/NamingConventionsConverters/SnakeCaseTests.cs
@@ -0,0 +1,30 @@
+﻿using CopyMatch.NamingConventionsConverters;
+using Xunit;
+
+namespace CopyMatch.Tests.NamingConventionsConverters
+{
+    public class SnakeCaseTests
+    {
+        [Theory]
+        [InlineData("snake case", "snake_case")]
+        [InlineData("snake", "snake")]
+        public static void StringToThisTest(string input, string expected)
+        {
+            SnakeCase cc = new SnakeCase();
+            string result = cc.StringToThis(input);
+
+            Assert.Equal(result, expected);
+        }
+
+        [Theory]
+        [InlineData("snake_case", "snake case")]
+        [InlineData("snake", "snake")]
+        public static void ThisToStringTest(string input, string expected)
+        {
+            SnakeCase cc = new SnakeCase();
+            string result = cc.ThisToString(input);
+
+            Assert.Equal(result, expected);
+        }
+    }
+}
