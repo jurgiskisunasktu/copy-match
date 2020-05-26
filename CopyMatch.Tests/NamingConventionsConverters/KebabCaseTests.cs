1f507e91f7f35ddfdf69ec0a0c92672e21bff64d
Jurgis Kisunas <jurgis.kisunas@elinta.lt>
2020-05-26 23:44:12 +0300

    Kebab case tests

---
+++ CopyMatch.Tests/NamingConventionsConverters/KebabCaseTests.cs
@@ -0,0 +1,30 @@
+﻿using CopyMatch.NamingConventionsConverters;
+using Xunit;
+
+namespace CopyMatch.Tests.NamingConventionsConverters
+{
+    public class KebabCaseTests
+    {
+        [Theory]
+        [InlineData("kebab case", "kebab-case")]
+        [InlineData("kebab", "kebab")]
+        public static void StringToThisTest(string input, string expected)
+        {
+            KebabCase cc = new KebabCase();
+            string result = cc.StringToThis(input);
+
+            Assert.Equal(result, expected);
+        }
+
+        [Theory]
+        [InlineData("kebab-case", "kebab case")]
+        [InlineData("kebab", "kebab")]
+        public static void ThisToStringTest(string input, string expected)
+        {
+            KebabCase cc = new KebabCase();
+            string result = cc.ThisToString(input);
+
+            Assert.Equal(result, expected);
+        }
+    }
+}
